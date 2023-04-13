a77197eab3b894883200fea2465deaa7bc6f4a13
Norman Köster <nkoester@itemis.com>
2023-04-13 11:17:08 +0200

    Add documentation for subproject platform enforcement

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,16 @@ plugins {
     kotlin("kapt") version "1.8.20" apply false
     id("com.specificlanguages.mps") version "1.5.0" apply false
     id ("com.github.node-gradle.node") version "3.2.1" apply false
+    // we use the java plugin so that we can use the implementation in the
+    // subprojects block to declare modelix platform dependencies for all
+    // subprojects
     java
 }
 
 subprojects {
+    // as this sample is a giant monorepo, we can enforce the modelix platform
+    // to be used everywhere here. as a result we will not have to pick
+    // versions for individual modelix artifacts anywhere
     apply(plugin = "java")
     val modelix_platform_version: String by project
     dependencies {
