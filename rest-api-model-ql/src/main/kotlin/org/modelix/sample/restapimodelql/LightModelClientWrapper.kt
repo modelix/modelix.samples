865da7d4bfb8f4b5c2830ba68e0227829ebe9d32
Norman Köster <nkoester@itemis.com>
2023-05-02 16:35:46 +0200

    Fix query formatting

--- rest-api-model-ql/src/main/kotlin/org/modelix/sample/restapimodelql/LightModelClientWrapper.kt
+++ rest-api-model-ql/src/main/kotlin/org/modelix/sample/restapimodelql/LightModelClientWrapper.kt
@@ -34,19 +34,19 @@ class LightModelClientWrapper(
         // the modelQL query
         this.lightModelClient.changeQuery(buildModelQuery {
             // we traverse from the root (a repository)
-                root {
-                    // to all modules
-                    children("modules") {
-                        // selecting the ones that contain our desired model name
-                        whereProperty("name").contains(modelNameSubstring)
-                        children("models") {
-                            // and extract all root nodes, so Rooms and Courses, and their descendants
-                            children("rootNodes") {
-                                descendants { }
-                            }
+            root {
+                // to all modules
+                children("modules") {
+                    // selecting the ones that contain our desired model name
+                    whereProperty("name").contains(modelNameSubstring)
+                    children("models") {
+                        // and extract all root nodes, so Rooms and Courses, and their descendants
+                        children("rootNodes") {
+                            descendants { }
                         }
                     }
                 }
+            }
         })
         logger.info("Connection successful")
     }
